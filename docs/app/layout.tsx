451f910735fc4f72252d3b3a31258758285f9348
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-20 15:57:41 +0200

    docs: add analytics

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import { RootProvider } from 'fumadocs-ui/provider'
 import { Inter } from 'next/font/google'
 import './global.css'
@@ -26,6 +27,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
       </body>
     </html>
   )
