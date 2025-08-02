860ae616c4c5432c069b8049e6df480d65b51ed1
Valerii Strilets <strilets.v@novapay.ua>
2025-08-03 01:18:29 +0300

    chore: update packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,6 +7,7 @@ export default antfu(
     rules: {
       'node/prefer-global/process': 'off',
       'react/no-use-context': 'off',
+      'style/indent': 'off', // temp due to bug
     },
     ignores: ['**/.source/**'],
   },
